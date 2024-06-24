9c59bf95fde289f9612e92f850576a398fa7821a
Sarah O Connor <sarahaoconnor10@gmail.com>
2024-06-24 16:08:34 +0100

    added palindromeCheckerV2

--- HelloWorld/src/main/java/org/microplacements/Palindrome.java
+++ HelloWorld/src/main/java/org/microplacements/Palindrome.java
@@ -1,33 +1,46 @@
 package org.microplacements;
 
 public class Palindrome {
-    public Palindrome() {
-        
-    }
+    public Palindrome() { }
     
     public boolean palindromeChecker(char[] word) {
+        if(word.length == 0) { 
+            return true; 
+        }
         char[] reversedWord = new char[word.length];
-        int j = 0;
-        boolean palindrome = false;
-        if(word.length == 0) {
-            palindrome = true;
+        int i = word.length - 1;
+        for(char letter : word) {
+            reversedWord[i] = letter;
+            i--;
         }
-        
-        for(int i = word.length -1; i >= 0; i--) {
-            reversedWord[j] = word[i];
-            j++;
-        }
-        
-        for(int i = word.length -1; i >= 0; i--) {
+
+        for(i = word.length -1; i >= 0; i--) {
             if(word[i] != reversedWord[i]) {
-                palindrome = false;
-                break;
+                return false;
             }
-            palindrome = true;
+        }   
+       
+        return true;
+    }
+    public boolean palindromeCheckerV2(char[] word) {
+        if(word.length < 2) { 
+            return true; 
+        }
+        int numIterations = getNumIterations(word.length);
+        
+        for(int i = 0; i < numIterations; i++) {
+            if(word[i] != word[word.length - 1 - i]) {
+                return false;
+            }
         }
         
-        return palindrome;
+     return true;
     }
     
-    
+    private int getNumIterations(int len) {
+        if(len % 2 != 0) {
+            return (len - 1) / 2;
+        }
+        return len / 2;
+    }
 }
